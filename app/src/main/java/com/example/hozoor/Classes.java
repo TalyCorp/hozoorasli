2a5223c6adc4d1828f928d5dff9bdf01ebcd4c44
kingAmir <talyCorp@gmail.com>
2019-07-07 08:12:40 -0700

    hi amir

--- app/src/main/java/com/example/hozoor/Classes.java
+++ app/src/main/java/com/example/hozoor/Classes.java
@@ -5,17 +5,25 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Classes extends AppCompatActivity {
+
+    TextView add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classes);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -23,8 +31,35 @@ public class Classes extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                        AlertDialog.Builder  builder=new AlertDialog.Builder ( Classes.this);
+                        View view1 = getLayoutInflater ().inflate ( R.layout.custom_dialog_add_calss, null );
+
+                final EditText New_kelas=(EditText) view1.findViewById( R.id.name_class);
+                TextView afzodn = (TextView)view1.findViewById ( R.id.btn_afzodn_new_class );
+
+                afzodn.setOnClickListener ( new View.OnClickListener () {
+                    @Override
+                    public void onClick(View view) {
+
+                        if (!New_kelas.getText ().toString ().isEmpty ())
+                        {
+                            Toast.makeText ( Classes.this, ":)", Toast.LENGTH_SHORT ).show ();
+                        }
+                        else
+                        {
+                            Toast.makeText ( Classes.this, ":(", Toast.LENGTH_SHORT ).show ();
+                        }
+                    }
+                } );
+
+                builder.setView ( view1 );
+                AlertDialog dialog = builder.create ();
+                dialog.show ();
+
             }
         });
     }
